366bc2c86322df769eb9bb2868507ef7b7e1c3a3
RobinAnd-Py <109841354+RobinAnd-py@users.noreply.github.com>
2022-07-23 14:40:19 +0930

    Add files via upload

---
+++ First_project.py
@@ -0,0 +1,8 @@
+print("Hello World! I'm a robot!")
+print("Can you please give me a name?")
+Name1 = input()
+print("OOOOOOOooooOh, I love my new name " + Name1)
+print("Can you please give me a favourite colour?")
+colour = input()
+print("OOOOOOOooooOh, I love my favourite colour " + colour)
+print("Thanks for playing!")
